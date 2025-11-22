3a4b288aca7515e35d3c175523d33a7793f81482
keisuke.mitsumune <keisuke@yuiarch.com>
2025-11-22 18:22:40 +0900

    add skip_final_snapshot parameter to aws_rds_cluster

--- infra/iac/envs/rds.tf
+++ infra/iac/envs/rds.tf
@@ -7,6 +7,7 @@ resource "aws_rds_cluster" "this" {
   master_password         = "ThisIsPlainTextPassword0987!"
   backup_retention_period = 7
   preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
   db_subnet_group_name    = aws_db_subnet_group.this.name
   vpc_security_group_ids = [
     aws_security_group.ec2.id,
