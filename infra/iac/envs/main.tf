1190b03d94c199874e4892426c08229d4489c3ca
keisuke.mitsumune <keisuke@yuiarch.com>
2025-11-23 15:55:28 +0900

    set database password by manage_master_user_password

--- infra/iac/envs/main.tf
+++ infra/iac/envs/main.tf
@@ -15,3 +15,59 @@ provider "aws" {
   alias  = "virginia"
   region = "us-east-1"
 }
+
+locals {
+  general = {
+    service_name = "yui-lab"
+    region       = "ap-northeast-1"
+  }
+  route53 = {
+    domain_name     = "mitsumune.click"
+    sub_domain_name = "lab"
+    zone_id         = "Z05000742TPFCB5Y6EWEG"
+  }
+  kms = {
+    alias_name = "alias/yui-kms-key"
+  }
+  s3 = {
+    bucket_name         = "${local.route53.sub_domain_name}.${local.route53.domain_name}"
+    statics_dir         = "../../../apps/statics"
+    statics_path_prefix = ""
+    content_types = {
+      css  = "text/css"
+      html = "text/html"
+      js   = "application/javascript"
+      json = "application/json"
+      txt  = "text/plain"
+      xml  = "application/xml"
+      svg  = "image/svg+xml"
+      png  = "image/png"
+    }
+  }
+  cloudfront = {
+    log_prefix = "access_log/"
+  }
+  alb = {
+    idle_timeout = 900
+  }
+  ec2 = {
+    ami              = "ami-0d5f5a4eaac1481cb"
+    instance_type    = "t3.nano"
+    root_volume_size = "10"
+    root_volume_type = "gp3"
+    ssh_cidr_blocks = [
+      "133.200.162.32/32",
+    ]
+  }
+  db = {
+    cluster = {
+      engine          = "aurora-mysql"
+      engine_version  = "8.0.mysql_aurora.3.10.1"
+      database_name   = "yui_db"
+      master_username = "admin"
+    }
+    instance = {
+      instance_class = "db.t3.medium"
+    }
+  }
+}
--- infra/iac/envs/rds.tf
+++ infra/iac/envs/rds.tf
@@ -1,14 +1,16 @@
 resource "aws_rds_cluster" "this" {
-  cluster_identifier      = "${local.general.service_name}-aurora"
-  engine                  = local.db.cluster.engine
-  engine_version          = local.db.cluster.engine_version
-  database_name           = local.db.cluster.database_name
-  master_username         = local.db.cluster.master_username
-  master_password         = "ThisIsPlainTextPassword0987!"
-  backup_retention_period = 7
-  preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = true
-  db_subnet_group_name    = aws_db_subnet_group.this.name
+  cluster_identifier = "${local.general.service_name}-aurora"
+  engine             = local.db.cluster.engine
+  engine_version     = local.db.cluster.engine_version
+  database_name      = local.db.cluster.database_name
+  master_username    = local.db.cluster.master_username
+  # master_password         = "ThisIsPlainTextPassword0987!"
+  manage_master_user_password   = true
+  master_user_secret_kms_key_id = aws_kms_key.this.id
+  backup_retention_period       = 7
+  preferred_backup_window       = "07:00-09:00"
+  skip_final_snapshot           = true
+  db_subnet_group_name          = aws_db_subnet_group.this.name
   vpc_security_group_ids = [
     aws_security_group.ec2.id,
   ]
@@ -32,4 +34,45 @@ resource "aws_rds_cluster_instance" "reader" {
   engine_version       = aws_rds_cluster.this.engine_version
   publicly_accessible  = false
   db_subnet_group_name = aws_db_subnet_group.this.name
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "null_resource" "show_rds_password" {
+  depends_on = [
+    aws_rds_cluster.this
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOT
+#!/bin/bash
+
+REGION="${local.general.region}"
+ACCOUNT_ID="${data.aws_caller_identity.current.account_id}"
+KMS_KEY_ID="${aws_kms_alias.this.target_key_id}"
+CLUSTER_NAME="${aws_rds_cluster.this.cluster_identifier}"
+
+SECRET_ARN=$(aws secretsmanager list-secrets \
+    --region "$REGION" \
+    --query "SecretList[?KmsKeyId=='arn:aws:kms:$REGION:$ACCOUNT_ID:key/$KMS_KEY_ID' && contains(Description, '$CLUSTER_NAME')].ARN" \
+    --output text)
+
+if [ -z "$SECRET_ARN" ]; then
+  echo "Error: Secret not found for cluster $CLUSTER_NAME"
+  exit 1
+fi
+
+DB_PASSWORD=$(aws secretsmanager get-secret-value \
+    --region "$REGION" \
+    --secret-id "$SECRET_ARN" \
+    --query 'SecretString' \
+    --output text | jq -r '.password')
+
+echo ""
+echo "===== RDS MASTER PASSWORD for $CLUSTER_NAME ====="
+echo "$DB_PASSWORD"
+echo "================================================="
+echo ""
+EOT
+  }
 }
